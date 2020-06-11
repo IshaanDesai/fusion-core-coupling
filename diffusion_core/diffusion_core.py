5605507c61fdd3bb043344284d711e38fcd458a8
ishaandesai <ishaandesai@gmail.com>
2020-06-11 20:48:59 +0200

    Moving to simpler neighbor search and difference scheme accessing

--- diffusion_core/diffusion_core.py
+++ diffusion_core/diffusion_core.py
@@ -21,11 +21,13 @@ config = Config(problem_config_file)
 
 # Mesh setup
 mesh = Mesh(problem_config_file)
-nx, ny = mesh.get_n_points_axiswise()
+nr, ntheta = mesh.get_n_points_axiswise()
 rmin, rmax = config.get_rmin(), config.get_rmax()
 
 # Definition of field variable
-u = np.zeros((nx, ny))
+u = np.zeros((nr, ntheta+2))
+# Copy of field variable for output and post-processing
+u_out = np.zeros((nr, ntheta))
 
 # Initialising Gaussian blob as initial condition of field
 x_center, y_center = config.get_xb_yb()
@@ -39,17 +41,13 @@ for l in range(mesh.get_n_points_grid()):
 
     i, j = mesh.get_i_j_from_index(mesh_ind)
     u[i, j] = gaussx*gaussy
-    assert(u[i, j] >= 0), "gaussx = {}, gauss_y = {}, for ({},{})".format(gaussx, gaussx, i, j)
 
 # Setup Dirichlet boundary conditions at inner and outer edge of the torus
-for i in range(nx):
-    for j in range(ny):
+for i in range(nr):
+    for j in range(1, ntheta+1):
         # Point type is a list and not a numpy array so it requires different index accessing
-        if mesh.get_point_type(i, j) == MeshVertexType.GHOST:
+        if mesh.get_point_type(i, j-1) == MeshVertexType.GHOST:
             u[i, j] = 0
-
-# Write data at initial condition
-write_vtk(u, mesh, 0)
 
 # Get parameters from config and mesh modules
 diffc_perp = config.get_diffusion_coeff()
@@ -57,46 +55,58 @@ print("Diffusion coefficient = {}".format(diffc_perp))
 dt = config.get_dt()
 print("dt = {}".format(dt))
 n_t, n_out = config.get_n_timesteps(), config.get_n_output()
-dr, dtheta = mesh.get_r_spacing(), mesh.get_theta_spacing()
+dr = mesh.get_r_spacing()
+
+# Calculate d_theta (spacing in theta direction)
+dtheta = np.zeros(nr)
+r_c = rmin - dr
+for k in range(nr):
+    dtheta[k] = mesh.get_theta_spacing(r_c)
+    r_c += dr
+assert(rmax < r_c < rmax + 2*dr), "theta spacing does not match mesh geometry"
 
 # Check the CFL Condition for Diffusion Equation
 cfl_r = dt*diffc_perp / (dr*dr)
 print("CFL Coefficient with radial param = {}. Must be less than 0.5".format(cfl_r))
 assert(cfl_r < 0.5)
 
-cfl_theta = dt*diffc_perp / (rmin*rmin*dtheta*dtheta)
+cfl_theta = dt*diffc_perp / (rmin*rmin*np.amin(dtheta)*np.amin(dtheta))
 print("CFL Coefficient with theta param = {}. Must be less than 0.5".format(cfl_theta))
 assert(cfl_theta < 0.5)
 
+# Calculate radius values at each grid point
+r_val = np.zeros((nr, ntheta+2))
+for i in range(nr):
+    for j in range(1, ntheta+1):
+        mesh_ind = mesh.get_index_from_i_j(i, j-1)
+        r_val[i, j] = mesh.get_r(mesh_ind)
+r_val[:, 0] = r_val[:, 1]
+r_val[:, ntheta+1] = r_val[:, ntheta]
+
 # Time loop
 for n in range(n_t):
-    for l in range(mesh.get_n_points_grid()):
-        mesh_ind = mesh.grid_to_mesh_index(l)
-        l_i, l_j = mesh.get_i_j_from_index(mesh_ind)
-        r = mesh.get_r(mesh_ind)
-
-        ltheta_minus_i, ltheta_minus_j = mesh.get_neighbor_i_j(mesh_ind, -1, 0)
-        ltheta_plus_i, ltheta_plus_j = mesh.get_neighbor_i_j(mesh_ind, 1, 0)
-        lr_minus_i, lr_minus_j = mesh.get_neighbor_i_j(mesh_ind, 0, -1)
-        lr_plus_i, lr_plus_j = mesh.get_neighbor_i_j(mesh_ind, 0, 1)
-
-        rminus_ind, rplus_ind = mesh.get_neighbor_index(mesh_ind, 0, -1), mesh.get_neighbor_index(mesh_ind, 0, 1)
-        rminus, rplus = mesh.get_r(rminus_ind), mesh.get_r(rplus_ind)
-        rplus_half, rminus_half = (rplus + r) / 2, (rminus + r) / 2
-
-        # Staggered scheme to evaluate derivatives in radial direction
-        du_perp = (rplus_half*(u[lr_plus_i, lr_plus_j] - u[l_i, l_j]) / dr - rminus_half*(u[lr_minus_i, lr_minus_j] - u[l_i, l_j]) / dr) / (r*dr)
-
-        # Second order central difference components in theta direction
-        du_perp += (u[ltheta_minus_i, ltheta_minus_j] + u[ltheta_plus_i, ltheta_plus_j] - 2*u[l_i, l_j]) / (r*r*dtheta*dtheta)
-
-        du_perp = du_perp*dt*diffc_perp
-
-        u[l_i, l_j] += du_perp
-
     if n % n_out == 0:
-        write_vtk(u, mesh, n)
+        write_vtk(u_out, mesh, n)
+
+    # Assign values to ghost cells for periodicity in theta direction
+    u[:, 0] = u[:, ntheta]
+    u[:, ntheta+1] = u[:, 1]
+
+    # Iterate over all grid points in a Cartesian grid fashion
+    for i in range(1, nr-1):
+        for j in range(1, ntheta+1):
+            # Staggered grid scheme to evaluate derivatives in radial direction
+            du_perp = (r_val[i+1, j]*(u[i+1, j] - u[i, j]) / dr - r_val[i-1, j]*(u[i, j] - u[i-1, j]) / dr) / (r_val[i, j]*dr)
+
+            # Second order central difference components in theta direction
+            du_perp += (u[i, j-1] + u[i, j+1] - 2*u[i, j]) / (r_val[i, j]*r_val[i, j]*dtheta[i]*dtheta[i])
+
+            du_perp = du_perp*dt*diffc_perp
+            u[i, j] += du_perp
+            u_out[i, j-1] = u[i, j]
 
     n += 1
+    if n % 100 == 0:
+        print("Elapsed time = {}. Field sum = {}".format(n*dt, u_out.sum()))
 
 # End
--- diffusion_core/mesh_2d.py
+++ diffusion_core/mesh_2d.py
@@ -45,7 +45,7 @@ class Mesh:
         self._mesh_i, self._mesh_j = None, None  # Initialized in create_mesh function
 
         self._mesh_count, self._grid_count, self._ghost_count = 0, 0, 0
-        self._nx, self._ny = 0, self._config.get_theta_points()
+        self._r_points = 0.0
 
         self._create_mesh()
 
@@ -54,29 +54,27 @@ class Mesh:
         theta_spacing = 2*math.pi / self._theta_points
 
         # Adding 2 extra radial vertex rows as ghost point layers for boundary conditions
-        r_vertices = int(round(r_v)) + 2
+        self._r_points = int(round(r_v)) + 2
 
-        self._nx = r_vertices
+        print("r_points = {}, theta_points = {}".format(self._r_points, self._theta_points))
+        print("Polar mesh has {} points".format(self._r_points*self._theta_points))
 
-        print("r_vertices = {}, rho_vertices = {}".format(r_vertices, self._theta_points))
-        print("Polar mesh has {} points".format(r_vertices*self._theta_points))
-
-        self._polar_coords_r = np.zeros((r_vertices, self._theta_points))
-        self._polar_coords_theta = np.zeros((r_vertices, self._theta_points))
-        self._cart_coords_x = np.zeros((r_vertices, self._theta_points))
-        self._cart_coords_y = np.zeros((r_vertices, self._theta_points))
-        self._mesh_index = np.zeros((r_vertices, self._theta_points))
-        self._mesh_i = np.zeros(r_vertices*self._theta_points)
-        self._mesh_j = np.zeros(r_vertices*self._theta_points)
+        self._polar_coords_r = np.zeros((self._r_points, self._theta_points))
+        self._polar_coords_theta = np.zeros((self._r_points, self._theta_points))
+        self._cart_coords_x = np.zeros((self._r_points, self._theta_points))
+        self._cart_coords_y = np.zeros((self._r_points, self._theta_points))
+        self._mesh_index = np.zeros((self._r_points, self._theta_points), dtype=int)
+        self._mesh_i = np.zeros(self._r_points*self._theta_points, dtype=int)
+        self._mesh_j = np.zeros(self._r_points*self._theta_points, dtype=int)
 
         # https://stackoverflow.com/questions/6667201/how-to-define-a-two-dimensional-array-in-python
-        self._vertex_type = [[0 for x in range(self._theta_points)] for y in range(r_vertices)]
+        self._vertex_type = [[0 for x in range(self._theta_points)] for y in range(self._r_points)]
 
         mesh_index_grid, mesh_index_ghost = [], []
 
         r_count = self._rmin - self._r_spacing
         theta_count, k = 0, 0
-        for i in range(r_vertices):
+        for i in range(self._r_points):
             for j in range(self._theta_points):
                 self._polar_coords_r[i, j] = r_count
                 self._polar_coords_theta[i, j] = theta_count
@@ -109,10 +107,10 @@ class Mesh:
         self._mesh_index_of_ghost = np.array(mesh_index_ghost)
 
     def get_i_j_from_index(self, index):
-        return int(self._mesh_i[index]), int(self._mesh_j[index])
+        return self._mesh_i[index], self._mesh_j[index]
 
     def get_index_from_i_j(self, ind_i, ind_j):
-        return int(self._mesh_index[ind_i, ind_j])
+        return self._mesh_index[ind_i, ind_j]
 
     def get_x(self, index):
         i, j = self.get_i_j_from_index(index)
@@ -146,7 +144,7 @@ class Mesh:
         return self._ghost_count
 
     def get_n_points_axiswise(self):
-        return self._nx, self._ny
+        return self._r_points, self._theta_points
 
     def get_point_type(self, ind_i, ind_j):
         return self._vertex_type[ind_i][ind_j]
@@ -154,39 +152,15 @@ class Mesh:
     def get_r_spacing(self):
         return self._r_spacing
 
-    def get_theta_spacing(self):
-        return 2*math.pi / self._theta_points
-
-    def get_neighbor_i_j(self, index, x_dir, y_dir):
-        neigh_i = int(self._mesh_i[index] + x_dir)
-        neigh_j = int(self._mesh_j[index] + y_dir)
-
-        # Handling periodicity in the angular direction
-        if neigh_j >= self._ny:
-            return neigh_i, 1
-        elif neigh_j <= 0:
-            return neigh_i, self._ny-1
-        else:
-            return neigh_i, neigh_j
-
-    def get_neighbor_index(self, index, x_dir, y_dir):
-        neigh_i = int(self._mesh_i[index] + x_dir)
-        neigh_j = int(self._mesh_j[index] + y_dir)
-
-        # Handling periodicity in the angular direction
-        if neigh_j >= self._ny:
-            neigh_j = 1
-        elif neigh_j <= 0:
-            neigh_j = self._ny - 1
-
-        return self.get_index_from_i_j(neigh_i, neigh_j)
+    def get_theta_spacing(self, radius):
+        return 2*math.pi*radius / (2*self._theta_points)
 
     def polar_to_cartesian(self, r, rho):
         x_coord = r*math.cos(rho)
         y_coord = r*math.sin(rho)
         if x_coord < self._r_spacing:
             x_coord = 0
-        if y_coord < self.get_theta_spacing():
+        if y_coord < self.get_theta_spacing(r):
             y_coord = 0
 
         return x_coord, y_coord
