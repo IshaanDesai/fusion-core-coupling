59bd5b7148ad3c7f9d5bb47520ba02b9edb00624
ishaandesai <ishaandesai@gmail.com>
2020-08-03 13:50:21 +0200

    Adding second order Neumann BC implementation

--- diffusion_core/modules/boundary.py
+++ diffusion_core/modules/boundary.py
@@ -15,7 +15,7 @@ class BoundaryType(enum.Enum):
     NEUMANN = 3
 
 
-def set_bnd_vals(mesh, data, field):
+def set_bnd_vals_dirichlet(mesh, data, field):
     nr, ntheta = mesh.get_n_points_axiswise()
     counter = 0
     for i in range(nr):
@@ -23,4 +23,17 @@ def set_bnd_vals(mesh, data, field):
             # Point type is a list and not a numpy array so it requires different index accessing
             if mesh.get_point_type(i, j - 1) == MeshVertexType.GHOST:
                 field[i, j] = data[counter]
+                counter += 1
+
+
+def set_bnd_vals_neumann(mesh, fluxes, field):
+    nr, ntheta = mesh.get_n_points_axiswise()
+    dr = mesh.get_r_spacing()
+    counter = 0
+    for i in range(nr):
+        for j in range(ntheta):
+            # Point type is a list and not a numpy array so it requires different index accessing
+            if mesh.get_point_type(i, j - 1) == MeshVertexType.GHOST:
+                # Second order formulation of Neumann BC using two inner points (only valid for Core)
+                field[i, j] = (4/3)*field[i-1, j] - (1/3)*field[i-2, j] - (2/3)*dr*fluxes[counter]
                 counter += 1
