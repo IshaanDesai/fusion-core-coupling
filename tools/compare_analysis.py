18c37d7137155bb6e3f9faf9f7c4d3b512b69b63
Ishaan Desai <idesai@draco02.mpcdf.mpg.de>
2020-09-01 10:05:44 +0200

    Changing output file names

--- tools/compare_analysis.py
+++ tools/compare_analysis.py
@@ -10,56 +10,60 @@ import matplotlib.pyplot as plt
 def read_data(res_str, code_name, n_t):
     # Read data from CSV files of specified code
     points, field = [], []
-    with open('./output_' + res_str + '/' + code_name + '_' + str(n_t) + '.csv', 'r') as file:
+    filename = './output_' + res_str + '/' + code_name + '_' + str(n_t) + '.csv'
+    with open(filename, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
-            points.append([row[0], row[1]])
-            field.append(row[2])
+            points.append([float(row[0]), float(row[1])])
+            field.append(float(row[2]))
+    print("Reading from file {} successful".format(filename))
 
     return np.array(points), np.array(field)
 
 
-def compare_data_2d(mesh_res, edge_ts, core_ts, ref_points, ref_data):
-    edge_pts, edge_f = read_data(str(mesh_res), 'parallax', edge_ts)
-    core_pts, core_f = read_data(str(mesh_res), 'polar', core_ts)
+def compare_data_2d(mesh_res, tstamp, ref_points, ref_data):
+    edge_pts, edge_f = read_data(str(mesh_res), 'parallax', tstamp)
+    core_pts, core_f = read_data(str(mesh_res), 'polar', tstamp)
     interp_edge_f = griddata(ref_points, ref_data, edge_pts, method='cubic')
     interp_core_f = griddata(ref_points, ref_data, core_pts, method='cubic')
 
-    assert interp_edge_f.shape == ref_field.shape
-    assert interp_core_f.shape == ref_field.shape
+    print("edge_f.shape = {}, core_f.shape = {}".format(edge_f.shape, core_f.shape))
+    assert interp_edge_f.shape == edge_f.shape
+    assert interp_core_f.shape == core_f.shape
 
-    i, error_edge = 0, 0
-    for u_edge, u_ref in edge_f, interp_edge_f:
-        error_edge += abs(u_edge[i] - u_ref[i])/abs(u_ref[i])
-        i += 1
+    error_edge = 0
+    for u_ref in interp_edge_f:
+        if u_ref != 0:
+            for u_edge in edge_f:
+                error_edge += abs(u_edge - u_ref)/abs(u_ref)
 
-    print("Error between reference and edge case with mesh res ({}) = {}".format(res, err_edge[res]))
+    print("Error between reference and edge case with mesh res ({}) = {}".format(res, error_edge))
 
-    i, error_core = 0, 0
-    for u_core, u_ref in core_f, interp_edge_f:
-        error_core += abs(u_core[i] - u_ref[i]) / abs(u_ref[i])
-        i += 1
+    error_core = 0
+    for u_ref in interp_edge_f:
+        if u_ref != 0:
+            for u_core in core_f:
+                error_core += abs(u_core - u_ref) / abs(u_ref)
 
-    print("Error between reference and core case with mesh res ({}) = {}".format(res, err_core[res]))
+    print("Error between reference and core case with mesh res ({}) = {}".format(res, error_core))
 
     return error_edge, error_core
 
 
 # Finest resolution (3) is reference result
-ref_points, ref_field = read_data('ref', 'polar', 63999)
+ref_points, ref_field = read_data('ref', 'polar', 64101)
 
 # For each mesh resolution, fit data by interpolation and calculate error by L2 norm
 mesh_res = 3
 # Time stamps for each mesh resolutions in PARALLAX and Polar Code to match names of output files
-parallax_ts = [10000, 10000, 10000]
-polar_ts = [40000, 50000, 63999]
 
 err_edge = np.zeros(mesh_res)
 err_core = np.zeros(mesh_res)
 err_coupling = np.zeros(mesh_res)
 for res in range(mesh_res):
-    err_edge[res], err_core[res] = compare_data_2d(res, parallax_ts[res], polar_ts[res], ref_points, ref_field)
-    err_coupling_1, err_coupling_2 = compare_data_2d('coupling_'+str(res), parallax_ts[res], polar_ts[res], ref_points, ref_field)
+    print("Comparing mesh resolution number: {}".format(res))
+    err_edge[res], err_core[res] = compare_data_2d(res, res, ref_points, ref_field)
+    err_coupling_1, err_coupling_2 = compare_data_2d('coupling_'+str(res), res, ref_points, ref_field)
     err_coupling[res] = err_coupling_1 + err_coupling_2
 
 # Plotting
