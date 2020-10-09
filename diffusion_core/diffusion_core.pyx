4552320ac53e7c82d22902c77c69042bd9116c86
Ishaan Desai <idesai@draco02.mpcdf.mpg.de>
2020-10-09 07:10:23 +0200

    Initial commit for diffusion in diverted geometry

--- diffusion_core/diffusion_core.pyx
+++ diffusion_core/diffusion_core.pyx
@@ -13,6 +13,7 @@ from diffusion_core.modules.mms cimport MMS
 import math
 import time
 import logging
+import netCDF4 as nc
 
 class Diffusion:
     def __init__(self):
@@ -28,39 +29,51 @@ class Diffusion:
         # Iterators
         cdef Py_ssize_t i, j
 
-        # Mesh setup
-        mesh = Mesh(config)
-        nr, ntheta = config.get_r_points(), config.get_theta_points()
-        rmin, rmax = config.get_rmin(), config.get_rmax()
+        # Read metric coefficient NetCDF file
+        ds = nc.Dataset('./polars.nc')
 
-        # Create MMS module object
-        mms = MMS(config, mesh)
+        # Mesh setup
+        nrho, ntheta = ds.dimensions['nrho'], ds.dimensions['ntheta']
+        assert nrho == config.get_r_points()
+        assert ntheta == config.get_theta_points()
+
+        # Get geometrical variables from NetCDF file
+        rho_np, theta_np = np.array(ds['rho'][:]), np.array(ds['theta'][:])
+        cdef double [:, ::1] rho = rho_np
+        cdef double [:, ::1] theta = theta_np
+        xpol_np, ypol_np = np.array(ds['xpol'][:]), np.array(ds['ypol'][:])
+        cdef double [:, ::1] xpol = xpol_np
+        cdef double [:, ::1] ypol = ypol_np
+
+        # Get Jacobian from NetCDF file
+        jac_np = np.array(ds['jacobian'][:])
+        cdef double [:, ::1] jac = jac_np
+
+        # Get metric coefficients from NetCDF file
+        g_rhorho_np, g_rhotheta_np = np.array(ds['g_rhorho'][:]), np.array(ds['g_rhotheta'][:])
+        cdef double [:, ::1] g_rhorho = g_rhorho_np
+        cdef double [:, ::1] g_rhotheta = g_rhotheta_np
+        g_thetatheta_np, g_phiphi_np = np.array(ds['g_thetatheta'][:]), np.array(ds['g_phiphi'][:])
+        cdef double [:, ::1]  g_thetatheta = g_thetatheta_np
+        cdef double [:, ::1] g_phiphi = g_phiphi_np
 
         # Field variable array
-        u_np = np.zeros((nr, ntheta), dtype=np.double)
+        u_np = np.zeros((nrho, ntheta), dtype=np.double)
         cdef double [:, ::1] u = u_np
         # Field delta change array
-        du_perp_np = np.zeros((nr, ntheta), dtype=np.double)
+        du_perp_np = np.zeros((nrho, ntheta), dtype=np.double)
         cdef double [:, ::1] du_perp = du_perp_np
 
         # Initializing custom initial state (sinosoidal)
-        for l in range(mesh.get_n_points_grid()):
-            mesh_ind = mesh.grid_to_mesh_index(l)
-            radialp = mesh.get_r(mesh_ind)
-            thetap = mesh.get_theta(mesh_ind)
+        for i in range(nrho):
+            for j in range(ntheta):
+                u[i, j] = mms.init_mms(rho[i], theta[j])
 
-            i, j = mesh.get_i_j_from_index(mesh_ind)
-            u[i, j] = mms.init_mms(radialp, thetap)
-
-        # Initialize boundary conditions at inner and outer edge of the torus
-        bndvals_wall = np.zeros((mesh.get_n_points_wall()))
-        bnd_wall = Boundary(config, mesh, bndvals_wall, u, BoundaryType.DIRICHLET, MeshVertexType.BC_WALL)
-        bndvals_core = np.zeros((mesh.get_n_points_core()))
-        bnd_core = Boundary(config, mesh, bndvals_core, u, BoundaryType.DIRICHLET, MeshVertexType.BC_CORE)
-
-        # Reset boundary conditions according to MMS ansatz
-        bnd_wall.set_bnd_vals_mms(u, 0)
-        bnd_core.set_bnd_vals_mms(u, 0)
+        # Set boundary conditions (Dirichlet)
+        for i in range(nrho):
+            for j in range(ntheta):
+                if i == 0 or i == nrho-1:
+                    u[i, j] = 0.0
 
         # Get parameters from config and mesh modules
         diffc_perp = config.get_diffusion_coeff()
@@ -71,38 +84,28 @@ class Diffusion:
         cdef int n_t = int(t_total/dt)
         cdef int n_out = int(t_out/dt)
 
-        cdef double dr = mesh.get_r_spacing()
-        cdef double dtheta = mesh.get_theta_spacing()
+        cdef double drho = ds.globalattributes['drho']
+        cdef double dtheta = ds.globalattributes['dtheta']
 
-        # Calculate radius and theta values at each grid point
-        r_self_np = np.zeros((nr, ntheta), dtype=np.double)
-        cdef double [:, ::1] r_self = r_self_np
-        r_minus_np = np.zeros((nr, ntheta), dtype=np.double)
-        cdef double [:, ::1] r_minus = r_minus_np
-        r_plus_np = np.zeros((nr, ntheta), dtype=np.double)
-        cdef double [:, ::1] r_plus = r_plus_np
+        # Calculate rho and theta values at each grid point
+        rho_minus_np = np.zeros((nrho, ntheta), dtype=np.double)
+        cdef double [:, ::1] rho_minus = rho_minus_np
+        rho_plus_np = np.zeros((nrho, ntheta), dtype=np.double)
+        cdef double [:, ::1] rho_plus = rho_plus_np
 
-        theta_self_np = np.zeros((nr, ntheta), dtype=np.double)
-        cdef double [:, ::1] theta_self = theta_self_np
-
-        for i in range(1, nr - 1):
+        for i in range(1, nrho - 1):
             for j in range(ntheta):
-                mesh_ind = mesh.get_index_from_i_j(i, j)
-                ind_minus = mesh.get_index_from_i_j(i - 1, j)
-                ind_plus = mesh.get_index_from_i_j(i + 1, j)
-                # r_(i,j) value
-                r_self[i, j] = mesh.get_r(mesh_ind)
-                # r_(i-1/2,j) value
-                r_minus[i, j] = (mesh.get_r(mesh_ind) + mesh.get_r(ind_minus)) / 2.0
+                # rho_(i,j) value
+                rho_self[i, j] = rho[i, j]
+                # rho_(i-1/2,j) value
+                rho_minus[i, j] = (rho[i-1, j] + rho[i, j]) / 2
                 # r_(i+1/2,j) value
-                r_plus[i, j] = (mesh.get_r(mesh_ind) + mesh.get_r(ind_plus)) / 2.0
-                # theta_(i,j) value
-                theta_self[i, j] = mesh.get_theta(mesh_ind)
+                r_plus[i, j] = (rho[i+1, j] + rho[i, j]) / 2
 
         # Check the CFL Condition for Diffusion Equation
-        cfl_r = dt * diffc_perp / (dr * dr)
-        self.logger.info('CFL Coefficient with radial param = %f. Must be less than 0.5', cfl_r)
-        cfl_theta = dt * diffc_perp / (np.mean(r_self) * np.mean(r_self) * dtheta * dtheta)
+        cfl_rho = dt * diffc_perp / (drho * drho)
+        self.logger.info('CFL Coefficient with radial param = %f. Must be less than 0.5', cfl_rho)
+        cfl_theta = dt * diffc_perp / (np.mean(rho) * np.mean(rho) * dtheta * dtheta)
         self.logger.info('CFL Coefficient with theta param = %f. Must be less than 0.5', cfl_theta)
         assert (cfl_r < 0.5)
         assert (cfl_theta < 0.5)
@@ -114,7 +117,7 @@ class Diffusion:
             for i in range(1, nr - 1):
                 # Calculating for points theta = 0
                 # Staggered grid scheme to evaluate derivatives in radial direction
-                du_perp[i, 0] = (r_plus[i, 0]*(u[i+1, 0] - u[i, 0]) - r_minus[i, 0]*(u[i, 0] - u[i-1, 0])) / (
+                du_perp[i, 0] = (rho_plus[i, 0]*(u[i+1, 0] - u[i, 0]) - rho_minus[i, 0]*(u[i, 0] - u[i-1, 0])) / (
                     r_self[i, 0]*dr*dr)
                 # Second order central difference components in theta direction
                 du_perp[i, 0] += (u[i, ntheta-1] + u[i, 1] - 2*u[i, 0]) / (r_self[i, 0]*r_self[i, 0]*dtheta*dtheta)
