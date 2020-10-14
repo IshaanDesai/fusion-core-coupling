cfbc6bd1c87ecb39fa356a105b37ede205a07a3b
ishaandesai <ishaandesai@gmail.com>
2020-10-14 16:53:50 +0200

    Working diverted geometry diffusion code

--- diffusion_core/diffusion_core.pyx
+++ diffusion_core/diffusion_core.pyx
@@ -7,7 +7,6 @@ cimport numpy as np
 cimport cython
 from diffusion_core.modules.output import write_vtk, write_csv
 from diffusion_core.modules.config import Config
-from diffusion_core.modules.mms cimport MMS
 import math
 import time
 import logging
@@ -31,14 +30,14 @@ class Diffusion:
         ds = nc.Dataset('./polars.nc')
 
         # Mesh setup
-        nrho, ntheta = ds.dimensions['nrho'], ds.dimensions['ntheta']
-        assert nrho == config.get_r_points()
+        nrho, ntheta = ds.dimensions['nrho'].size, ds.dimensions['ntheta'].size
+        assert nrho == config.get_rho_points()
         assert ntheta == config.get_theta_points()
 
         # Get geometrical variables from NetCDF file
         rho_np, theta_np = np.array(ds['rho'][:]), np.array(ds['theta'][:])
-        cdef double [:, ::1] rho = rho_np
-        cdef double [:, ::1] theta = theta_np
+        cdef double [::1] rho = rho_np
+        cdef double [::1] theta = theta_np
         xpol_np, ypol_np = np.array(ds['xpol'][:]), np.array(ds['ypol'][:])
         cdef double [:, ::1] xpol = xpol_np
         cdef double [:, ::1] ypol = ypol_np
@@ -51,101 +50,107 @@ class Diffusion:
         g_rr_np, g_rt_np = np.array(ds['g_rhorho'][:]), np.array(ds['g_rhotheta'][:])
         cdef double [:, ::1] g_rr = g_rr_np
         cdef double [:, ::1] g_rt = g_rt_np
-        g_tt_np, = np.array(ds['g_thetatheta'][:])
+        g_tt_np = np.array(ds['g_thetatheta'][:])
         cdef double [:, ::1]  g_tt = g_tt_np
 
         # Field variable array
-        u_np = np.zeros((nrho, ntheta), dtype=np.double)
+        u_np = np.zeros((ntheta, nrho), dtype=np.double)
         cdef double [:, ::1] u = u_np
         # Field delta change array
-        du_np = np.zeros((nrho, ntheta), dtype=np.double)
+        du_np = np.zeros((ntheta, nrho), dtype=np.double)
         cdef double [:, ::1] du = du_np
 
-        # Initializing custom initial state (sinosoidal)
-        for i in range(nrho):
-            for j in range(ntheta):
-                u[i, j] = mms.init_mms(rho[i], theta[j])
+        # Initializing gaussian blob
+        xb, yb = config.get_xb_yb()
+        wxb, wyb = config.get_wxb_wyb()
+        for i in range(ntheta):
+            for j in range(nrho):
+                x = xpol[i, j]
+                y = ypol[i, j]
+                gaussx = math.exp(-(x-xb)*(x-xb) / (wxb*wxb))
+                gaussy = math.exp(-(y-yb)*(y-yb) / (wyb*wyb))
+                u[i, j] = gaussx*gaussy
 
         # Set boundary conditions (Dirichlet)
-        for i in range(nrho):
-            for j in range(ntheta):
-                if i == 0 or i == nrho-1:
+        for i in range(ntheta):
+            for j in range(nrho):
+                if j == 0 or j == nrho-1:
                     u[i, j] = 0.0
 
         # Get parameters from config and mesh modules
-        diffc_perp = config.get_diffusion_coeff()
-        self.logger.info('Diffusion coefficient = %f', diffc_perp)
+        diffc = config.get_diffusion_coeff()
+        self.logger.info('Diffusion coefficient = %f', diffc)
         cdef double dt = config.get_dt()
         self.logger.info('dt = %f', dt)
         t_total, t_out = config.get_total_time(), config.get_t_output()
         cdef int n_t = int(t_total/dt)
         cdef int n_out = int(t_out/dt)
 
-        cdef double drho = ds.globalattributes['drho']
-        cdef double dtheta = ds.globalattributes['dtheta']
+        cdef double drho = ds.getncattr('drho')
+        cdef double dtheta = ds.getncattr('dtheta')
 
         # Check the CFL Condition for Diffusion Equation
-        cfl_rho = dt * diffc_perp / (drho * drho)
+        cfl_rho = dt * diffc / (drho * drho)
         self.logger.info('CFL Coefficient with radial param = %f. Must be less than 0.5', cfl_rho)
-        cfl_theta = dt * diffc_perp / (np.mean(rho) * np.mean(rho) * dtheta * dtheta)
+        cfl_theta = dt * diffc / (np.mean(rho) * np.mean(rho) * dtheta * dtheta)
         self.logger.info('CFL Coefficient with theta param = %f. Must be less than 0.5', cfl_theta)
-        assert (cfl_r < 0.5)
+        assert (cfl_rho < 0.5)
         assert (cfl_theta < 0.5)
 
         # Time loop
         cdef double u_sum
         for n in range(n_t):
             # Assign values to ghost cells for periodicity in theta direction
-            jp = [ntheta-2, ntheta-1, 0, 1]
-            for i in range(1, nrho - 1):
-                for j in range(1, 3):
+            ip = [ntheta-2, ntheta-1, 0, 1]
+            for i in range(1, 3):
+                for j in range(1, nrho-1):
                     # Staggered grid for rho-rho diagonal term
-                    du[i, jp[j]] = ((jac[i+1, jp[j]] + jac[i, jp[j]])*(g_rr[i+1, jp[j]] + g_rr[i, jp[j]])*(u[i+1, jp[j]] - u[i, jp[j]]) -
-                        (jac[i, jp[j]] + jac[i-1, jp[j]])*(g_rr[i, jp[j]] + g_rr[i-1, jp[j]])*(u[i, jp[j]] - u[i-1, jp[j]])) / (4*drho*drho)
+                    du[ip[i], j] = ((jac[ip[i], j+1] + jac[ip[i], j])*(g_rr[ip[i], j+1] + g_rr[ip[i], j])*(u[ip[i], j+1] - u[ip[i], j]) -
+                        (jac[ip[i], j] + jac[ip[i], j-1])*(g_rr[ip[i], j] + g_rr[ip[i], j-1])*(u[ip[i], j] - u[ip[i], j-1])) / (4*drho*drho)
 
                     # Staggered grid for theta-theta diagonal term
-                    du[i, jp[j]] += ((jac[i, jp[j+1]] + jac[i, jp[j]])*(g_tt[i, jp[j+1]] + g_tt[i, jp[j]])*(u[i, jp[j+1]] - u[i, jp[j]]) -
-                        (jac[i, jp[j]] + jac[i, jp[j-1]])*(g_tt[i, jp[j]] + g_tt[i, jp[j-1]])*(u[i, jp[j]] - u[i-1, jp[j]])) / (4*dtheta*dtheta)
+                    du[ip[i], j] += ((jac[ip[i+1], j] + jac[ip[i], j])*(g_tt[ip[i+1], j] + g_tt[ip[i], j])*(u[ip[i+1], j] - u[ip[i], j]) -
+                        (jac[ip[i], j] + jac[ip[i-1], j])*(g_tt[ip[i], j] + g_tt[ip[i-1], j])*(u[ip[i], j] - u[ip[i-1], j])) / (4*dtheta*dtheta)
 
                     # Off-diagonal term rho-theta
-                    du[i, jp[j]] += (jac[i+1, jp[j]]*g_rt[i+1, jp[j]]*(u[i+1, jp[j+1]] - u[i+1, jp[j-1]]) -
-                        jac[i-1, jp[j]]*g_rt[i-1, jp[j]]*(u[i-1, jp[j+1]] - u[i-1, jp[j-1]])) / (4*drho*dtheta)
+                    du[ip[i], j] += (jac[ip[i], j+1]*g_rt[ip[i], j+1]*(u[ip[i+1], j+1] - u[ip[i-1], j+1]) -
+                        jac[ip[i], j-1]*g_rt[ip[i], j-1]*(u[ip[i+1], j-1] - u[ip[i-1], j-1])) / (4*drho*dtheta)
 
                     # Off-diagonal term theta-rho
-                    du[i, jp[j]] += (jac[i, jp[j+1]]*g_rt[i, jp[j+1]]*(u[i+1, jp[j+1]] - u[i-1, jp[j+1]]) -
-                        jac[i, jp[j-1]]*g_rt[i, jp[j-1]]*(u[i+1, jp[j-1]] - u[i-1, jp[j-1])) / (4*dtheta*drho)
+                    du[ip[i], j] += (jac[ip[i+1], j]*g_rt[ip[i+1], j]*(u[ip[i+1], j+1] - u[ip[i+1], j-1]) -
+                        jac[ip[i-1], j]*g_rt[ip[i-1], j]*(u[ip[i-1], j+1] - u[ip[i-1], j-1])) / (4*dtheta*drho)
 
-            # Iterate over all grid points in a Cartesian grid fashion
-            for i in range(1, nrho-1):
-                for j in range(1, ntheta-1):
+            # Iterate over all grid points
+            for i in range(1, ntheta-1):
+                for j in range(1, nrho-1):
                     # Staggered grid for rho-rho diagonal term
-                    du[i, j] = ((jac[i+1, j] + jac[i, j])*(g_rr[i+1, j] + g_rr[i, j])*(u[i+1, j] - u[i, j]) -
-                        (jac[i, j] + jac[i-1, j])*(g_rr[i, j] + g_rr[i-1,j])*(u[i, j] - u[i-1, j])) / (4*drho*drho)
+                    du[i, j] = ((jac[i, j+1] + jac[i, j])*(g_rr[i, j+1] + g_rr[i, j])*(u[i, j+1] - u[i, j]) -
+                        (jac[i, j] + jac[i, j-1])*(g_rr[i, j] + g_rr[i, j-1])*(u[i, j] - u[i, j-1])) / (4*drho*drho)
 
                     # Staggered grid for theta-theta diagonal term
-                    du[i, j] += ((jac[i, j+1] + jac[i, j])*(g_tt[i, j+1] + g_tt[i, j])*(u[i, j+1] - u[i, j]) -
-                        (jac[i, j] + jac[i, j-1])*(g_tt[i, j] + g_tt[i, j-1])*(u[i, j] - u[i-1, j])) / (4*dtheta*dtheta)
+                    du[i, j] += ((jac[i+1, j] + jac[i, j])*(g_tt[i+1, j] + g_tt[i, j])*(u[i+1, j] - u[i, j]) -
+                        (jac[i, j] + jac[i-1, j])*(g_tt[i, j] + g_tt[i-1, j])*(u[i, j] - u[i-1, j])) / (4*dtheta*dtheta)
 
                     # Off-diagonal term rho-theta
-                    du[i, j] += (jac[i+1, j]*g_rt[i+1, j]*(u[i+1, j+1] - u[i+1, j-1]) -
-                        jac[i-1, j]*g_rt[i-1, j]*(u[i-1, j+1] - u[i-1, j-1])) / (4*drho*dtheta)
-
-                    # Off-diagonal term theta-rho
                     du[i, j] += (jac[i, j+1]*g_rt[i, j+1]*(u[i+1, j+1] - u[i-1, j+1]) -
-                        jac[i, j-1]*g_rt[i, j-1]*(u[i+1, j-1] - u[i-1, j-1])) / (4*dtheta*drho)
+                        jac[i, j-1]*g_rt[i, j-1]*(u[i+1, j-1] - u[i-1, j-1])) / (4*drho*dtheta)
+
+                    # Off-diagonal term theta-rho
+                    du[i, j] += (jac[i+1, j]*g_rt[i+1, j]*(u[i+1, j+1] - u[i+1, j-1]) -
+                        jac[i-1, j]*g_rt[i-1, j]*(u[i-1, j+1] - u[i-1, j-1])) / (4*dtheta*drho)
 
             # Update scheme
-            for i in range(1, nr - 1):
-                for j in range(ntheta):
+            for i in range(ntheta):
+                for j in range(nrho):
                     u[i, j] += dt*diffc*du[i, j] / jac[i, j]
 
             if n%n_out == 0 or n == n_t-1:
                 write_csv(u, xpol, ypol, n+1)
-                write_vtk(u, xpol, ypol n+1)
+                write_vtk(u, xpol, ypol, n+1)
                 self.logger.info('VTK file output written at t = %f', n*dt)
                 u_sum = 0
-                for i in range(nrho):
-                    for j in range(0, ntheta):
+                for i in range(ntheta):
+                    for j in range(nrho):
                         u_sum += u[i, j]
 
                 self.logger.info("Elapsed time = {}  || Field sum = {}".format(n*dt, u_sum/(nrho*ntheta)))
--- diffusion_core/modules/output.py
+++ diffusion_core/modules/output.py
@@ -10,37 +10,28 @@ import csv
 
 def write_vtk(field, xpol, ypol, t):
     filename = "field_out_{}".format(t)
-    nrho, ntheta = xpol.size, ypol.size
+    nrho, ntheta = 50, 200
     nz = 1
-    x_out = np.zeros((nrho, ntheta, nz))
-    y_out = np.zeros((nrho, ntheta, nz))
-    z_out = np.zeros((nrho, ntheta, nz))
-    point_type = np.zeros((nrho, ntheta, nz))
-    field_out = np.zeros((nrho, ntheta, nz))
+    x_out = np.zeros((ntheta, nrho, nz))
+    y_out = np.zeros((ntheta, nrho, nz))
+    z_out = np.zeros((ntheta, nrho, nz))
+    point_type = np.zeros((ntheta, nrho, nz))
+    field_out = np.zeros((ntheta, nrho, nz))
 
-    counter = 0
-    for i in range(nrho):
-        for j in range(ntheta):
-            x_out[i, j, 0] = 
-            y_out[i, j, 0] = mesh.get_y(counter)
+    for i in range(ntheta):
+        for j in range(nrho):
+            x_out[i, j, 0] = xpol[i, j]
+            y_out[i, j, 0] = ypol[i, j]
             z_out[i, j, 0] = 0
-            if mesh.get_point_type(i, j) == MeshVertexType.BC_CORE or mesh.get_point_type(i, j) == MeshVertexType.BC_WALL:
-                point_type[i, j, 0] = 1
-            elif mesh.get_point_type(i, j) == MeshVertexType.GRID:
-                point_type[i, j, 0] = 0
             field_out[i, j, 0] = field[i, j]
-            counter += 1
 
     gridToVTK("./output/"+filename, x_out, y_out, z_out, pointData={"field": field_out, "type": point_type})
 
 
-def write_csv(field, mesh, n):
-    nr, ntheta = mesh.get_n_points_axiswise()
-    counter = 0
+def write_csv(field, xpol, ypol, n):
+    nrho, ntheta = 50, 200
     with open('./output/polar_'+str(n)+'.csv', mode='w') as file:
         file_writer = csv.writer(file, delimiter=',')
-        for i in range(nr):
-            for j in range(ntheta):
-                file_writer.writerow([mesh.get_x(counter), mesh.get_y(counter), field[i, j]])
-                counter += 1
-
+        for i in range(ntheta):
+            for j in range(nrho):
+                file_writer.writerow([xpol[i, j], ypol[i, j], field[i, j]])
