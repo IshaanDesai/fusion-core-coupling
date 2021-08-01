d510af79331fbf9c5fe66abbf28fc8958f10703f
idesai <ishaan.desai@ipp.mpg.de>
2021-08-01 16:59:19 +0200

    Updating preCICE build script for Cobra

--- build-precice.sh
+++ build-precice.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
 module purge
-module load gcc/9 impi/2019.7 cmake petsc-real anaconda
+module load gcc/9 impi/2019.7 cmake/3.18 petsc-real boost/1.74
 module list
-
-# adapt to eigen path
-export Eigen3_ROOT=$HOME/eigen-3.3.7
 
 rm -rf build
 mkdir -p build && cd build
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PWD/installed -DPRECICE_PETScMapping=OFF \
-  -DPRECICE_PythonActions=OFF -DMPI_CXX_COMPILER=mpigcc -DPYTHON_EXECUTABLE=$(which python) \
-  -DBoost_NO_BOOST_CMAKE=TRUE -DBoost_NO_SYSTEM_PATHS=TRUE -DBOOST_ROOT:PATHNAME=/u/idesai/boost_install -DBoost_LIBRARY_DIRS:FILEPATH=/u/idesai/boost_install/lib ..
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PWD/installed -DMPI_CXX_COMPILER=mpigcc \
+   -DCMAKE_BUILD_TYPE=Debug -DPRECICE_PythonActions=OFF ..
 make -j20
 
-# correct RPATH in shared object
-LIB=$PWD/libprecice.so
-#echo Current RPATH: $(patchelf --print-rpath $LIB)
-patchelf --set-rpath /usr/lib64:$(patchelf --print-rpath $LIB) $LIB
-#echo RPATH after patching: $(patchelf --print-rpath $LIB)
+#make test_base
+#make install
 
-make test_base
-make install
